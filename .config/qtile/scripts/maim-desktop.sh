729ebe461207621d599466aa729d2ac4631666f9
slats-ru <128977309+slats-ru@users.noreply.github.com>
2024-03-31 14:47:46 +0300

    Add some dots

--- .config/qtile/scripts/maim-desktop.sh
+++ .config/qtile/scripts/maim-desktop.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-maim /home/slats/Images/Screenshots/$(date +%Y-%b-%d--%H-%M-%S_maim | tr A-Z a-z).png
+maim /mnt/data/Images/Screenshots/$(date +%Y-%b-%d--%H-%M-%S_maim | tr A-Z a-z).png
 notify-send "Screenshot taken"
+mpv --no-video /home/slats/.config/qtile/resources/foto.wav
