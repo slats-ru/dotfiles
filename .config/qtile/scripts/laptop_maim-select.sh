67f91721a8a5670ebb8c7f02cb0b4e0e9f0c6ee8
slats-ru <128977309+slats-ru@users.noreply.github.com>
2024-04-01 22:07:02 +0300

    Add some dots

---
+++ .config/qtile/scripts/laptop_maim-select.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+maim --select --hidecursor /home/slats/Images/Screenshots/$(date +%Y-%b-%d--%H-%M-%S_maim | tr A-Z a-z).png
+notify-send "Screenshot taken"
+mpv --no-video /home/slats/.config/qtile/resources/foto.wav
