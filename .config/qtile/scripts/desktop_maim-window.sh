b575791f81f1dceff6d405caa0d9107a735b7bb1
slats-ru <128977309+slats-ru@users.noreply.github.com>
2024-04-06 14:21:24 +0300

    Add some dots

--- .config/qtile/scripts/desktop_maim-window.sh
+++ .config/qtile/scripts/desktop_maim-window.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-maim --window $(xdotool getactivewindow) /home/slats/Images/Screenshots/$(date +%Y-%b-%d--%H-%M-%S_maim | tr A-Z a-z).png
+maim --window $(xdotool getactivewindow) /mnt/data/Images/Screenshots/$(date +%Y-%b-%d--%H-%M-%S_maim | tr A-Z a-z).png
 notify-send "Screenshot taken"
 mpv --no-video /home/slats/.config/qtile/resources/foto.wav
